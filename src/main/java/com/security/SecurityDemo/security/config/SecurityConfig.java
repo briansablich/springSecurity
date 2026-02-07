5468649f4bd06359e17a90d1c9f70e74ebd6565e
Brian_Sablich <sablichbrian@yahoo.com.ar>
2026-02-07 18:24:55 -0300

    Added authentication

--- src/main/java/com/security/SecurityDemo/security/config/SecurityConfig.java
+++ src/main/java/com/security/SecurityDemo/security/config/SecurityConfig.java
@@ -2,10 +2,24 @@
 package com.security.SecurityDemo.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -14,13 +28,67 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(csrf -> csrf.disable()) // Recomendado deshabilitar para pruebas iniciales
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/holanoseg").permitAll(); // URL pública
-                    auth.anyRequest().authenticated();             // Todo lo demás requiere login
-                })
-                .formLogin(form -> form.permitAll()) // Habilita el formulario de login visual
+                .csrf(csrf -> csrf.disable())
                 .httpBasic(basic -> Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))// Recomendado deshabilitar para pruebas iniciales
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(HttpMethod.GET, "/holanoseg").permitAll();
+                    auth.requestMatchers(HttpMethod.GET, "/holaseg").hasAuthority("READ");
+                    auth.anyRequest().denyAll();             // Todo lo demás requiere login
+                })
                 .build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        //provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService () {
+        List userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.withUsername("flauta")
+                .password("123") // esto si no está codificado, sino, tiene que seguir el algoritmo de codificación
+                .roles("ADMIN")
+                .authorities("CREATE", "READ", "UPDATE", "DELETE")
+                .build());
+
+        userDetailsList.add(User.withUsername("rocky")
+                .password("123") // esto si no está codificado, sino, tiene que seguir el algoritmo de codificación
+                .roles("USER")
+                .authorities("READ")
+                .build());
+
+        userDetailsList.add(User.withUsername("satelital")
+                .password("123") // esto si no está codificado, sino, tiene que seguir el algoritmo de codificación
+                .roles("USER")
+                .authorities("UPDATE")
+                .build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
+
+
+
 }
